458727e0e75481c11b2984d4e1a80b1ed68d7bac
Lasoliate <495998692@qq.com>
2016-11-29 09:34:25 +0800

    整合代码

--- app/src/main/java/com/phone1000/martialstudyself/constants/HttpUrl.java
+++ app/src/main/java/com/phone1000/martialstudyself/constants/HttpUrl.java
@@ -8,4 +8,10 @@ public class HttpUrl {
     public static final String HOME_URL = "http://m.wushu520.com/Home/IndexForAPP";
 
     public static final String HEADER_URL = "www.wushu520.com";
+
+    public static final String COMMENT_URL = "http://m.wushu520.com/Topic/ListForAPP?page=";
+    public static final String COMMENT_ITEM = "http://m.wushu520.com/Topic/InfoForAPP/";
+    public static final String COMMENT_COMMENT_START = "http://m.wushu520.com/Topic/GetCommentList?id=";
+    public static final String COMMENT_COMMENT_END = "&page=1&reply_type=1";
+
 }
